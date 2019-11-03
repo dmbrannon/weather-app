9b11795507f349ddad0593a0a95a82ebfa03037b
Dana Brannon <danabrannon@danas-air.attlocal.net>
2019-11-03 17:51:57 -0600

    Initial commit, implement basic UI

---
+++ dmb3738_assignment7/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dmb3738_assignment7
+//
+//  Created by Dana Brannon on 11/3/19.
+//  Copyright © 2019 Dana Brannon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
