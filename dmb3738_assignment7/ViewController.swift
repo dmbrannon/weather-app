ac09e9f8788c4a1c05e72a97bc38dde2a9d82947
Dana Brannon <danabrannon@danas-air.attlocal.net>
2019-11-03 18:29:09 -0600

    Connect UI components to code

--- dmb3738_assignment7/ViewController.swift
+++ dmb3738_assignment7/ViewController.swift
@@ -9,12 +9,67 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var cityField: UITextField!
+    @IBOutlet weak var stateField: UITextField!
+    @IBOutlet weak var checkConditions: UIButton!
+    
+    @IBOutlet weak var weatherImage: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    
+    @IBOutlet weak var CLOUDCOVER: UILabel!
+    @IBOutlet weak var HUMIDITY: UILabel!
+    @IBOutlet weak var PRESSURE: UILabel!
+    @IBOutlet weak var PRECIPITATION: UILabel!
+    @IBOutlet weak var WIND: UILabel!
+    
+    @IBOutlet weak var cloudLabel: UILabel!
+    @IBOutlet weak var humidityLabel: UILabel!
+    @IBOutlet weak var pressureLabel: UILabel!
+    @IBOutlet weak var precipitationLabel: UILabel!
+    @IBOutlet weak var windLabel: UILabel!
+    
+    @IBOutlet weak var NOTFOUND: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Hide data labels initially
+        weatherImage.isHidden = true
+        temperatureLabel.isHidden = true
+        
+        CLOUDCOVER.isHidden = true
+        HUMIDITY.isHidden = true
+        PRESSURE.isHidden = true
+        PRECIPITATION.isHidden = true
+        WIND.isHidden = true
+        
+        cloudLabel.isHidden = true
+        humidityLabel.isHidden = true
+        pressureLabel.isHidden = true
+        precipitationLabel.isHidden = true
+        windLabel.isHidden = true
+        
+        NOTFOUND.isHidden = true
     }
 
-
+    // MARK: Actions
+    @IBAction func getData(_ sender: Any) {
+        // Unhide data labels
+        weatherImage.isHidden = false
+        temperatureLabel.isHidden = false
+        
+        CLOUDCOVER.isHidden = false
+        HUMIDITY.isHidden = false
+        PRESSURE.isHidden = false
+        PRECIPITATION.isHidden = false
+        WIND.isHidden = false
+        
+        cloudLabel.isHidden = false
+        humidityLabel.isHidden = false
+        pressureLabel.isHidden = false
+        precipitationLabel.isHidden = false
+        windLabel.isHidden = false
+    }
+    
 }
 
